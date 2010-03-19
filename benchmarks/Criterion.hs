83062bc4bce5402f273679c41f3bc2aa2bcaf78e
Chris Done <chrisdone@chris-dones-macbook-pro.local>
2010-03-19 18:16:30 +0000

    added .ghci for criterion, basic criterion test, updated cabal file to include crition executable

--- benchmarks/Criterion.hs
+++ benchmarks/Criterion.hs
@@ -1,10 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
 import Criterion.Main (defaultMain,bench,whnf)
-fib :: Int -> Int
-fib 0 = 0
-fib 1 = 1
-fib n = fib (n-1) * fib (n-2)
+import Text.BlazeHtml.Html
+import Text.BlazeHtml.Render.HtmlIO
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
 
 main = defaultMain [
-        bench "fib 10" $ whnf fib 10
-       ,bench "fib 30" $ whnf fib 30
+       -- ,bench "fib 30" $ whnf fib 30
+       bench "addAttributes [(\"class\",\"demo\")]" $ renderHtmlIO T.putStr $ addAttributes [("class","demo")] $ renderLeafElement "p"
        ]

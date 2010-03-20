6f3362b88bca82bad0011dd74045b74e73846fd9
Frederick Ross <github.20.madhadron@spamgourmet.com>
2010-03-20 10:48:38 +0100

    Added <! and </ combinators.

--- src/Text/BlazeHtml/Html.hs
+++ src/Text/BlazeHtml/Html.hs
@@ -3,3 +3,11 @@ module Text.BlazeHtml.Html
     ) where
 
 import Text.BlazeHtml.Internal.Html hiding (modifyUnescapedAttributes)
+
+-- | The basic combinators for nesting HTMl and setting attributes are
+(<!) :: Html h => (h -> h) -> Attributes -> h -> h
+(<!) e attrs inner = addUnescapedAttributes attrs $ e inner
+ 
+-- | Set the inner html to a list of inner html elements
+(</) :: Html h => (h -> h) -> [h] -> h
+(</) e inner = e $ mconcat inner

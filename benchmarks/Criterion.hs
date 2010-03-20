5f530b3dffc0c16477b58e46054a9cada4543773
oliver <oliver.mueller@gmail.com>
2010-03-20 12:37:39 +0100

    added io file tests, renamed executable

--- benchmarks/Criterion.hs
+++ benchmarks/Criterion.hs
@@ -1,27 +1,33 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Criterion.Main (defaultMain,bench)
-
 import Control.Concurrent (forkIO)
 import Control.Monad (forever)
-import Data.Text()
+import Data.Text(Text)
 import qualified Data.Text.IO as T
-import Data.Text (Text)
-import qualified Data.Text as T
-import qualified Network as N
 import Network (PortID(PortNumber),Socket)
-import System.IO (Handle)
+import qualified Data.Text as T
+import Text.BlazeHtml.Html
+import Text.BlazeHtml.Render.HtmlIO
+import qualified Network as N
+import System.IO
+import Text.BlazeHtml.Render.HtmlText
 
 main :: IO ()
 main = N.withSocketsDo $ do
          server <- N.listenOn portID
          forkIO $ runSocketServer server
          handle <- N.connectTo "127.0.0.1" portID
+         fh <- openFile "./testfile.txt" WriteMode
          let contents = T.pack $ replicate 100000 'a'
-         defaultMain [bench "fileIO" fileIO
-                     ,bench "streamIO" $ streamIO handle contents]
+         defaultMain $ (fileIO fh) ++ [bench "streamIO" $ streamIO handle contents]
+         hClose fh
 
-fileIO :: IO ()
-fileIO = return ()
+fileIO h = [bench "file io test" $ fileTest h simpleText]
+
+fileTest :: Handle -> Text -> IO ()
+fileTest h t = T.hPutStr h t
+
+simpleText = renderHtmlText (addUnescapedAttributes [("class","demo")] $ renderLeafElement "p")
 
 -- | Run a simple fileIO test.
 streamIO :: Handle -> Text -> IO ()

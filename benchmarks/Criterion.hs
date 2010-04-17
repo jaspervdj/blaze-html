e6550009310076cb462c6e8040b22558a4f0804b
Jasper Van der Jeugt <jaspervdj@gmail.com>
2010-04-17 11:38:50 +0200

    Started out with an empty branch.

--- benchmarks/Criterion.hs
+++
@@ -1,8 +0,0 @@
-module Main where
-
-import Criterion.Main (defaultMain)
-import Criterion.Benchmarks
-
--- | No configuration, straight run sockets followed by file tests.
-main :: IO ()
-main = defaultMain benchmarks

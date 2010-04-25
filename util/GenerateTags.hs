8deaa35df4264c148d98a27df57da41e7ddb80e9
Jasper Van der Jeugt <jaspervdj@gmail.com>
2010-04-25 18:18:06 +0200

    Added function to actually produce our tag code.

--- util/GenerateTags.hs
+++ util/GenerateTags.hs
@@ -2,6 +2,36 @@
 --
 module GenerateTags where
 
+import Sanitize (sanitize)
+
+-- | Generate a function for an HTML tag that can be a parent.
+--
+parent :: String -> String
+parent tag = unlines
+    [ "-- | Combinator for the @<" ++ tag ++ ">@ element."
+    , "--"
+    , "-- Example:"
+    , "--"
+    , "-- > " ++ function ++ " $ text \"foo\""
+    , "--"
+    , "-- Result:"
+    , "--"
+    , "-- > <" ++ tag ++ ">foo</" ++ tag ++ ">"
+    , "--"
+    , function         ++ " :: Html -- ^ Inner HTML."
+    , justify function ++ " -> Html -- ^ Resulting HTML."
+    , function ++ " ="
+    , "    let begin, end :: ByteString"
+    , "        begin = \"<" ++ tag ++ "\""
+    , "        end = \"</" ++ tag ++ ">\""
+    , "        {-# NOINLINE begin #-}"
+    , "        {-# NOINLINE end #-}"
+    , "    in tag begin end"
+    , "{-# INLINE " ++ function ++ "#-}"
+    ]
+  where
+    function = sanitize tag
+    justify = flip replicate ' ' . length
 
 -- | A list of HTML strict non-leaf nodes.
 --

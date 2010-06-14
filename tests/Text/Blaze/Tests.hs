55cc4b14fdbeb2bc1aefc177fc37dcbfc0a9c588
Jasper Van der Jeugt <jaspervdj@gmail.com>
2010-06-14 10:33:52 +0200

    Check monoid laws using quickcheck.

--- tests/Text/Blaze/Tests.hs
+++ tests/Text/Blaze/Tests.hs
@@ -18,12 +18,46 @@ import Text.Blaze.Html5 hiding (map)
 import Text.Blaze.Html5.Attributes
 
 tests :: [Test]
-tests = []
+tests = [ testProperty "left identity Monoid law"  monoidLeftIdentity
+        , testProperty "right identity Monoid law" monoidRightIdentity
+        , testProperty "associativity Monoid law"  monoidAssociativity
+        , testProperty "mconcat Monoid law"        monoidConcat
+        ]
+
+-- | The left identity Monoid law.
+--
+monoidLeftIdentity :: Html a -> Bool
+monoidLeftIdentity h = mappend mempty h == h
+
+-- | The right identity Monoid law.
+--
+monoidRightIdentity :: Html a -> Bool
+monoidRightIdentity h = mappend h mempty == h
+
+-- | The associativity Monoid law.
+--
+monoidAssociativity :: Html a -> Html a -> Html a -> Bool
+monoidAssociativity x y z = mappend x (mappend y z) == mappend (mappend x y) z
+
+-- | Concatenation Monoid law.
+--
+monoidConcat :: [Html a] -> Bool
+monoidConcat xs = mconcat xs == foldr mappend mempty xs
+
+-- Show instance for the HTML type, so we can debug.
+--
+instance Show (Html a) where
+    show = show . renderHtml
+
+-- Eq instance for the HTML type, so we can compare the results.
+--
+instance Eq (Html a) where
+    h1 == h2 = renderHtml h1 == renderHtml h2
 
 -- Arbitrary instance for the HTML type.
 --
 instance Arbitrary (Html a) where
-    arbitrary = arbitraryHtml 5
+    arbitrary = arbitraryHtml 4
 
 -- | Auxiliary function for the arbitrary instance of the HTML type, used
 -- to limit the depth and size of the type.
@@ -32,7 +66,7 @@ arbitraryHtml :: Int           -- ^ Maximum depth.
               -> Gen (Html a)  -- ^ Resulting arbitrary HTML snippet.
 arbitraryHtml depth = do 
     -- Choose the size (width) of this element.
-    size <- choose (0, 10)
+    size <- choose (0, 3)
 
     -- Generate `size` new HTML snippets.
     children <- replicateM size arbitraryChild
@@ -47,7 +81,7 @@ arbitraryHtml depth = do
                        ++ [arbitraryParent | depth > 0]
 
         -- Generate some attributes for the child.
-        size <- choose (0, 3)
+        size <- choose (0, 4)
         attributes <- replicateM size arbitraryAttribute
         return $ foldl (!) child attributes
 

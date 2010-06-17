7b4785dcba567de9caf2521f0bc5b4e41172bd2a
Jasper Van der Jeugt <jaspervdj@gmail.com>
2010-06-17 22:27:24 +0200

    Self-documenting benchmarks (self-awareness soon)

--- benchmarks/Utf8Html.hs
+++ benchmarks/Utf8Html.hs
@@ -4,7 +4,8 @@
 module Utf8Html where
 
 import Data.Monoid (Monoid, mempty, mconcat, mappend)
-import Prelude hiding (div, id, head)
+import Prelude hiding (div, id)
+import qualified Prelude as P
 
 import Criterion.Main
 import Data.Binary.Builder (Builder, toLazyByteString)
@@ -14,28 +15,41 @@ import GHC.Exts (IsString, fromString)
 import qualified Data.ByteString.Lazy as LB
 import qualified Data.Text as T
 
-import Text.Blaze.Html4.Strict hiding (map)
-import Text.Blaze.Html4.Strict.Attributes hiding (title, rows)
+import Text.Blaze.Html5 hiding (map)
+import qualified Text.Blaze.Html5 as H
+import Text.Blaze.Html5.Attributes hiding (title, rows)
 
 main = defaultMain $ map benchHtml benchmarks
   where
-    benchHtml (HtmlBenchmark name f x) = bench name $ nf (LB.length . f) x
+    benchHtml (HtmlBenchmark name f x _) = bench name $ nf (LB.length . f) x
 
-data HtmlBenchmark = forall a. HtmlBenchmark
+data HtmlBenchmark = forall a b. HtmlBenchmark
     String                -- ^ Name.
     (a -> LB.ByteString)  -- ^ Rendering function.
     a                     -- ^ Data.
+    (Html b)              -- ^ Longer description.
 
 -- | List containing all benchmarks.
 --
 benchmarks :: [HtmlBenchmark]
 benchmarks =
-    [ HtmlBenchmark "bigTable"         bigTable       bigTableData
-    , HtmlBenchmark "basic"            basic          basicData
-    , HtmlBenchmark "wideTree"         wideTree       wideTreeData
-    , HtmlBenchmark "wideTreeEscaping" wideTree       wideTreeEscapingData
-    , HtmlBenchmark "deepTree"         deepTree       deepTreeData
-    , HtmlBenchmark "manyAttributes"   manyAttributes manyAttributesData
+    [ HtmlBenchmark "bigTable" bigTable bigTableData $
+        let h = showHtml $ length bigTableData
+            w = showHtml $ length $ P.head bigTableData
+        in "Rendering of a big (" >> h >> "x" >> w >> ") HTML table"
+    , HtmlBenchmark "basic" basic basicData
+        "A simple, small basic template with a few holes to fill in"
+    , HtmlBenchmark "wideTree" wideTree wideTreeData $
+        "A very wide tree (" >> showHtml (length wideTreeData) >> " elements)"
+    , HtmlBenchmark "wideTreeEscaping" wideTree wideTreeEscapingData $ do
+        "A very wide tree (" >> showHtml (length wideTreeData) >> " elements)"
+        " with lots of escaping"
+    , HtmlBenchmark "deepTree" deepTree deepTreeData $ do
+        "A really deep tree (" >> showHtml (length deepTreeData) >> " nested"
+        " elements)"
+    , HtmlBenchmark "manyAttributes" manyAttributes manyAttributesData $ do
+        "A single element with " >> showHtml (length manyAttributesData)
+        " attributes."
     ]
 
 rows :: Int
@@ -84,7 +98,7 @@ bigTable t = renderHtml $ table $ mconcat $ map row t
 basic :: (Text, Text, [Text])  -- ^ (Title, User, Items)
       -> LB.ByteString         -- ^ Result.
 basic (title', user, items) = renderHtml $ html $ do
-    head $ title $ text title'
+    H.head $ title $ text title'
     body $ do
         div ! id "header" $ (h1 $ text title')
         p $ "Hello, " `mappend` text user `mappend` text "!"

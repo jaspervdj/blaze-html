30cda486bc0932a5607fd0ff01c93c239ce852ce
Jasper Van der Jeugt <jaspervdj@gmail.com>
2010-07-14 16:57:38 +0200

    Use a pre-config hook to generate code

--- Setup.hs
+++ Setup.hs
@@ -1,3 +1,18 @@
 #!/usr/bin/env runhaskell
+
 import Distribution.Simple
-main = defaultMain
+import Distribution.Simple.Setup
+import Distribution.PackageDescription
+
+import System.Process (rawSystem)
+
+main :: IO ()
+main = defaultMainWithHooks hooks
+  where
+    hooks = simpleUserHooks { preConf = generateHtmlCombinators }
+
+generateHtmlCombinators :: Args -> ConfigFlags -> IO HookedBuildInfo
+generateHtmlCombinators _ _ = do
+    -- Portability needs to be tested.
+    _ <- rawSystem "runghc" ["-iutil", "util/GenerateHtmlVariant.hs"]
+    return emptyHookedBuildInfo

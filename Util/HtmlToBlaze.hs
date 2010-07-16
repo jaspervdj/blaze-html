7ba03be097229433acf4e3bea1684d673ce72d30
Jasper Van der Jeugt <jaspervdj@gmail.com>
2010-07-16 09:37:29 +0200

    Parse HTML to intermediate data structure first

--- Util/HtmlToBlaze.hs
+++ Util/HtmlToBlaze.hs
@@ -5,63 +5,73 @@ module HtmlToBlaze where
 import Data.List (intercalate)
 import Data.Maybe (listToMaybe, fromMaybe)
 import Data.Char (toLower, isSpace)
+import Control.Arrow (first)
 
 import Text.HTML.TagSoup
 
 import Util.GenerateHtmlCombinators
 
--- | Traverse the list of tags to produce the Haskell code. The tags should be
--- parsed with position information, so we can produce proper error messages.
+-- | Intermediate tree representation
 --
-traverse :: HtmlVariant   -- ^ HTML variant used
-         -> [String]      -- ^ Stack of open tags
-         -> Int           -- ^ Indentation level
-         -> [Tag String]  -- ^ Remaining tags to be parsed
-         -> IO ()         -- ^ No result yet
-traverse variant stack indent []
-    | null stack = putStrLn "OK, done"
-    | otherwise = putStrLn $ "Error: tags left open at the end: " ++ show stack
-traverse variant stack indent (TagPosition row column : x : xs) = case x of
-    -- Doctype is a special case!
-    TagOpen tag attrs -> if map toLower tag == "!doctype"
-        then do
-            putStrLn $ "DOCTYPE"
-            traverse variant stack indent xs
-        else do
-            -- TODO: Check for doctype, and leaf tags.
-            putStrLn $ "Open " ++ tag
-            -- Push the open tag on the stack and continue.
-            traverse variant (tag : stack) (indent + 1) xs
+data Html = Parent String [(String, String)] [Html]
+          | Text String
+          | Comment String
+          | Doctype
+          deriving (Show)
 
+-- | Traverse the list of tags to produce an intermediate representation of the
+-- HTML tree.
+--
+parseHtml :: [String]                -- ^ Stack of open tags
+          -> [Tag String]            -- ^ Tags to parse
+          -> ([Html], [Tag String])  -- ^ (Result, unparsed part)
+parseHtml stack []
+    | null stack = ([], [])
+    | otherwise = error $ "Error: tags left open at the end: " ++ show stack
+parseHtml stack (TagPosition row column : x : xs) = case x of
+    TagOpen tag attrs -> if toLower' tag == "!doctype"
+        then addHtml Doctype xs
+        else let (inner, t) = parseHtml (tag : stack) xs
+                 p = Parent (toLower' tag) (map (first toLower') attrs) inner
+             in addHtml p t
     -- The closing tag must match the stack.
-    TagClose tag -> if listToMaybe stack == Just tag
-        -- If the stack is matched, we happily continue.
-        then do
-            putStrLn $ "Close " ++ tag
-            traverse variant (tail stack) (indent - 1) xs
-        -- Otherwise, we throw an error.
-        else do
-            error $  "Line " ++ show row ++ ": " ++ show tag ++ " closed but "
-                  ++ show (listToMaybe stack) ++ " should be closed instead."
+    TagClose tag -> if listToMaybe stack == Just (toLower' tag)
+        then ([], xs)
+        else error $  "Line " ++ show row ++ ": " ++ show tag ++ " closed but "
+                   ++ show stack ++ " should be closed instead."
+    TagText text -> addHtml (Text text) xs
+    TagComment comment -> addHtml (Comment comment) xs
+    _ -> parseHtml stack xs
+  where
+    addHtml html xs' = let (l, r) = parseHtml stack xs'
+                       in (html : l, r)
 
-    -- We skip "empty" text nodes.
-    TagText text -> do
-        if all isSpace text
-            then putStrLn $ "Empty text (skipping)"
-            else putStrLn $ "Text " ++ text
-        traverse variant stack indent xs
+    toLower' = map toLower
 
-    TagComment comment -> do
-        putStrLn $ "Comment " ++ comment
-        traverse variant stack indent xs
+-- | Remove empty text from the HTML.
+--
+removeEmptyText :: [Html] -> [Html]
+removeEmptyText = map go . filter (not . isEmpty)
+  where
+    isEmpty (Text text) = all isSpace text
+    isEmpty _           = False
 
-    TagWarning str -> do
-        putStrLn $ "Warning " ++ str
-        traverse variant stack indent xs
+    go (Parent tag attrs inner) = Parent tag attrs $ removeEmptyText inner
+    go x = x
 
-    TagPosition _ _ -> do
-        putStrLn $ "Position information"
-        traverse variant stack indent xs
+-- | Convert the HTML to blaze code.
+--
+toBlaze :: Int -> [Html] -> String
+toBlaze indent = intercalate "\n"
+               . map (replicate (indent * 4) ' ' ++) . map toBlaze'
+  where
+    toBlaze' (Text text) = show text
+    toBlaze' (Comment comment) = "-- " ++ comment
+    toBlaze' Doctype = "docType"
+    toBlaze' (Parent tag _ inner)
+        | length inner == 1 = tag ++ " $ "
+                                  ++ dropWhile isSpace (toBlaze indent inner)
+        | otherwise = tag ++ " $\n" ++ toBlaze (indent + 1) inner
 
 parse' :: String -> [Tag String]
 parse' = parseTagsOptions parseOptions { optTagPosition = True }
@@ -70,5 +80,5 @@ test :: String
 test = unlines
     [ "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Frameset//EN\""
     , "    \"http://www.w3.org/TR/html4/frameset.dtd\">"
-    , "<html><head><title>lol</title></head><body><img /></body></html>"
+    , "<html><head><title><foo>lol<img /></foo></title></head><body><img /></body></html>"
     ]

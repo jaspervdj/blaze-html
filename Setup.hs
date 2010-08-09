cfe764a534b17e552887a4a36625592bd9b2d9c5
Jasper Van der Jeugt <jaspervdj@gmail.com>
2010-08-09 14:15:43 +0200

    Version bump

--- Setup.hs
+++ Setup.hs
@@ -1,17 +1,2 @@
-#!/usr/bin/env runhaskell
-
 import Distribution.Simple
-import Distribution.Simple.Setup
-import Distribution.PackageDescription
-
-import Util.GenerateHtmlCombinators (generateHtmlCombinators)
-
-main :: IO ()
-main = defaultMainWithHooks hooks
-  where
-    hooks = simpleUserHooks { preConf = preConf' }
-
-    -- Simple hook that generates the code for the HTML combinators.
-    preConf' _ _ = do
-        generateHtmlCombinators
-        return emptyHookedBuildInfo
+main = defaultMain

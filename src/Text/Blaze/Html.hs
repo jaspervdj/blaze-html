c65d721056a13b3f5c588dcc48f6ddba37b62b5b
Jasper Van der Jeugt <m@jaspervdj.be>
2012-04-23 15:47:25 +0200

    Add preEscapedToHtml

--- src/Text/Blaze/Html.hs
+++ src/Text/Blaze/Html.hs
@@ -2,11 +2,15 @@ module Text.Blaze.Html
     ( module Text.Blaze
     , Html
     , toHtml
+    , preEscapedToHtml
     ) where
 
 import Text.Blaze
 
 type Html = Markup
 
-toHtml :: ToMarkup a => a -> Markup 
+toHtml :: ToMarkup a => a -> Html
 toHtml = toMarkup
+
+preEscapedToHtml :: ToMarkup a => a -> Html
+preEscapedToHtml = preEscapedToMarkup

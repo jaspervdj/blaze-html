baca13839ff51315314edc3494003ce6a99dd253
Tyler Baum <37308097+AugmenTab@users.noreply.github.com>
2023-12-13 12:10:16 -0600

    Adds some missing elements that are present in the HTML documentation (#143)
    This adds the following elements:
    
    - bdi
    - data
    - dialog
    - picture
    - s
    - search
    - slot
    - template
    
    These elements are all supported in all major browsers. None of
    them are deprecated or experimental.

--- src/Util/GenerateHtmlCombinators.hs
+++ src/Util/GenerateHtmlCombinators.hs
@@ -401,18 +401,19 @@ html5 = HtmlVariant
     { version = ["Html5"]
     , docType = ["<!DOCTYPE HTML>"]
     , parents =
-        [ "a", "abbr", "address", "article", "aside", "audio", "b"
-        , "bdo", "blockquote", "body", "button", "canvas", "caption", "cite"
-        , "code", "colgroup", "command", "datalist", "dd", "del", "details"
-        , "dfn", "div", "dl", "dt", "em", "fieldset", "figcaption", "figure"
-        , "footer", "form", "h1", "h2", "h3", "h4", "h5", "h6", "head", "header"
-        , "hgroup", "html", "i", "iframe", "ins", "kbd", "label"
+        [ "a", "abbr", "address", "article", "aside", "audio", "b", "bdi", "bdo"
+        , "blockquote", "body", "button", "canvas", "caption", "cite", "code"
+        , "colgroup", "command", "data", "datalist", "dd", "del", "details"
+        , "dfn", "dialog", "div", "dl", "dt", "em", "fieldset", "figcaption"
+        , "figure", "footer", "form", "h1", "h2", "h3", "h4", "h5", "h6", "head"
+        , "header", "hgroup", "html", "i", "iframe", "ins", "kbd", "label"
         , "legend", "li", "main", "map", "mark", "menu", "meter", "nav"
         , "noscript", "object", "ol", "optgroup", "option", "output", "p"
-        , "pre", "progress", "q", "rp", "rt", "ruby", "samp", "script"
-        , "section", "select", "small", "span", "strong", "style", "sub"
-        , "summary", "sup", "table", "tbody", "td", "textarea", "tfoot", "th"
-        , "thead", "time", "title", "tr", "u", "ul", "var", "video"
+        , "picture", "pre", "progress", "q", "rp", "rt", "ruby", "s", "samp"
+        , "search", "script", "section", "select", "slot", "small", "span"
+        , "strong", "style", "sub", "summary", "sup", "table", "tbody", "td"
+        , "template", "textarea", "tfoot", "th", "thead", "time", "title", "tr"
+        , "u", "ul", "var", "video"
         ]
     , leafs =
         -- http://www.whatwg.org/specs/web-apps/current-work/multipage/syntax.html#void-elements
